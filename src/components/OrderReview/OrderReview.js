d0781b733291ee3998738094e2cb0e673f3151fb
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2021-10-01 19:57:21 +0600

    4th time

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import useProducts from '../../Hooks/useProducts';
+import useCart from '../../Hooks/useCart'
+import Cart from '../Cart/Cart'
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
-    const [products] = useProducts();
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products)
+    const handleRemove = key => {
+
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart)
+        
+
+    }
     return (
-        <div>
-            <h1>{products.length}</h1>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+
+                {
+
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product ={product}
+                        handleRemove = {handleRemove}
+                        ></ReviewItem>)
+
+                }
+
+            </div>
+            
+            <div className="cart-container">
+
+                <Cart cart={cart}></Cart>
+
+            </div>
+
+
         </div>
     );
 };
